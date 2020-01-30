8572183206ff14d96e3e19f07ac79cc0f61ae005
Samiicu <samuel.bocicu@gmail.com>
2020-01-31 01:14:00 +0200

    Remove unused code

--- PentruFacultate/app/src/main/java/com/example/samuel/pentrufacultate/managers/FirebaseReferencesManager.java
+++
@@ -1,9 +0,0 @@
-package com.example.samuel.pentrufacultate.managers;
-
-import com.google.firebase.database.DatabaseReference;
-
-import java.util.HashMap;
-
-public class FirebaseReferencesManager {
-    HashMap<String, DatabaseReference> firebaseReferences;
-}
